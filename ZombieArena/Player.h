df39fc9d600b181e86a6d5ff1d4327c7ebbd0dc8
kevin-brink <kbrink170@gmail.com>
2020-01-21 11:51:31 -0600

    Added player header file. Next will be adding the player source file

---
+++ ZombieArena/Player.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <sfml/Graphics.hpp>
+
+using namespace sf;
+
+class Player {
+private:
+	const float START_SPEED = 200;
+	const float START_HEALTH = 100;
+
+	Vector2f m_Position;
+	Sprite m_Sprite;
+	Texture m_Texture;
+	Vector2f m_Resolution;
+	IntRect m_Arena;
+	int m_TileSize;
+
+	bool m_UpPressed;
+	bool m_DownPressed;
+	bool m_LeftPressed;
+	bool m_RightPressed;
+
+	int m_Health;
+	int m_MaxHealth;
+	Time m_LastHit;
+
+	float m_Speed;
+
+public:
+	Player();
+	void spawn(IntRect arena, Vector2f resolution, int tileSize);
+	void resetPlayerStats();	// At the end of each game
+
+	bool hit(Time timeHit);		// Player hit by zombie
+
+	Time getLastHitTime();
+	FloatRect getPosition();
+	Vector2f getCenter();		// Get the center of the player
+	float getRotation();		// Which way the player is facing
+	Sprite getSprite();
+
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+
+	void stopLeft();
+	void stopRight();
+	void stopUp();
+	void stopDown();
+
+	void update(float elapsedTime, Vector2f mousePosition);
+
+	void upgradeSpeed();
+	void upgradeHealth();
+	void increaseHealthLevel(int amount);
+	int getHealth();
+};
