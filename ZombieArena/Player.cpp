b115cb97444ce32ac5d18ece6b61011d899f9ca0
kevin-brink <kbrink170@gmail.com>
2020-01-24 11:44:09 -0600

    Main view setup

--- ZombieArena/Player.cpp
+++ ZombieArena/Player.cpp
@@ -7,7 +7,7 @@ Player::Player() {
 	m_Health = START_HEALTH;
 	m_MaxHealth = START_HEALTH;
 
-	m_Texture.loadFromFile("graphic/player.png");
+	m_Texture.loadFromFile("graphics/player.png");
 	m_Sprite.setTexture(m_Texture);
 
 	m_Sprite.setOrigin(25, 25);
--- ZombieArena/ZombieArena.cpp
+++ ZombieArena/ZombieArena.cpp
@@ -1,5 +1,37 @@
 #include "stdafx.h"
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+
+using namespace sf;
 
 int main() {
+	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+
+	State state = State::GAME_OVER;
+
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	RenderWindow window(
+		VideoMode(resolution.x, resolution.y),
+		"Zombie Arena", Style::Fullscreen);
+
+	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	Clock clock;
+	Time gameTimeTotal;
+
+	Vector2f mouseWorldPosition;
+	Vector2f mouseScreenPosition;
+
+	Player player;
+	IntRect arena;
+
+	// Main Loop
+	while (window.isOpen()) {
+
+	}
+
 	return 0;
 }
